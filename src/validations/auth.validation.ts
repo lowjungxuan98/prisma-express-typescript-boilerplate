9444bad50609361a7a02d1cb879ff526b0f400b4
lowjungxuan <lowjungxuan@gmail.com>
2025-05-16 22:31:13 +0800

    Fix husky configuration

--- src/validations/auth.validation.ts
+++ src/validations/auth.validation.ts
@@ -4,6 +4,7 @@ import { password } from './custom.validation';
 const register = {
   body: Joi.object().keys({
     email: Joi.string().required().email(),
+    name: Joi.string().required(),
     password: Joi.string().required().custom(password)
   })
 };
